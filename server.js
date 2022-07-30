b8923ab20db84d5846830986965ac3b2e24b8f8f
Beatriz Andrade <beatrizrandrade@gmail.com>
2022-07-29 22:33:54 -0300

    correções de codigo

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app')
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT 
 
 
 app.listen(PORT, () => console.log(`Servidor funcionando na porta: ${PORT}`))
